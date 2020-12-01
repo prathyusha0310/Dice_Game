c17474d6da02fe4e3bd6dfe3e49d5e3c6b3cf99e
Ampolu Prathyusha <50418875+prathyusha0310@users.noreply.github.com>
2020-12-01 13:36:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,17 +3,17 @@ var ranLeftImage="dice"+randomNumber1+".png";
 var img1=document.querySelectorAll("img")[0];
 img1.setAttribute("src",ranLeftImage);
 
-var randomNumber2=Math.floor(Math.random()*6);
+var randomNumber2=Math.floor(Math.random()*6) + 1;
 var ranRightImage="dice"+randomNumber2+".png";
 var img2=document.querySelectorAll("img")[1];
 img2.setAttribute("src",ranRightImage);
 
 if(randomNumber2>randomNumber1){
-	document.querySelector("h1").innerHTML="Player 2 wins ! Better Luck next time player 1 :)";
+	document.querySelector("h1").innerHTML="🚩 Player 2 wins ! Better Luck next time player 1 :)";
 	
 }
 else if(randomNumber1>randomNumber2){
-	document.querySelector("h1").innerHTML="Player 1 wins ! Better Luck next time player 2 :)";
+	document.querySelector("h1").innerHTML="🚩 Player 1 wins ! Better Luck next time player 2 :)";
 	
 }
 else if(randomNumber1==randomNumber2){
